c83f2186c7894d838a60d0136b81d7f38a6d5d4f
lopes7890 <lopesallison414@gmail.com>
2025-03-29 15:07:32 -0300

    implementação inicial do app

--- TempoAgora/MainPage.xaml.cs
+++ TempoAgora/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace TempoAgora
+﻿using System.Threading.Tasks;
+using TempoAgora.Models;
+using TempoAgora.Services;
+
+namespace TempoAgora
 {
     public partial class MainPage : ContentPage
     {
@@ -9,16 +13,48 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                lbl_res.Text = "";
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Latidude: {t.lat} \n" +
+                            $"Longitude: {t.lon} \n" +
+                            $"Nascer do sol: {t.sunrise} \n" +
+                            $"Por do sol: {t.sunset} \n" +
+                            $"Temp Máx: {t.temp_max} \n" +
+                            $"Temp min: {t.temp_min} \n" +
+                            $"Velocidade do vento: {t.speed} \n" +
+                            $"Descrição: {t.description} \n" +
+                            $"Visibilidade: {t.visibility} \n";
+
+                        lbl_res.Text = dados_previsao;
+
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Sem dados de previsão";
+                    }
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "ok");
+            }
         }
     }
 
---
+++ TempoAgora/Services/DataService.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json.Linq;
+using TempoAgora.Models;
+
+namespace TempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            try
+            {
+                Tempo? t = null;
+
+                string chave = "ebda6f022e30cdd44dfcf37d7b267f09";
+                string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                             $"q={cidade}&units=metric&appid={chave}";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage resp = await client.GetAsync(url);
+
+                    if (resp.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    {
+                        await App.Current.MainPage.DisplayAlert("Erro", "Cidade não encontrada. Verifique o nome e tente novamente.", "OK");
+                        return null;
+                    }
+
+                    if (resp.IsSuccessStatusCode)
+                    {
+                        string json = await resp.Content.ReadAsStringAsync();
+
+                        var rascunho = JObject.Parse(json);
+
+                        //DateTime time = new();
+                        DateTime sunrise = DateTimeOffset.FromUnixTimeSeconds((long)rascunho["sys"]["sunrise"]).ToLocalTime().DateTime;
+                        DateTime sunset = DateTimeOffset.FromUnixTimeSeconds((long)rascunho["sys"]["sunset"]).ToLocalTime().DateTime;
+
+                        t = new()
+                        {
+                            lat = (double)rascunho["coord"]["lat"],
+                            lon = (double)rascunho["coord"]["lon"],
+                            description = (string)rascunho["weather"][0]["description"],
+                            main = (string)rascunho["weather"][0]["main"],
+                            temp_min = (double)rascunho["main"]["temp_min"],
+                            temp_max = (double)rascunho["main"]["temp_max"],
+                            speed = (double)rascunho["wind"]["speed"],
+                            visibility = (int)rascunho["visibility"],
+                            sunrise = sunrise.ToString(),
+                            sunset = sunset.ToString()
+                        }; // Fecha objeto Tempo
+                    } // Fecha if
+                } // Fecha using
+
+                return t;
+            }
+            catch (HttpRequestException)
+            {
+                await App.Current.MainPage.DisplayAlert("Sem conexão", "Verifique sua conexão com a internet e tente novamente.", "OK");
+                return null;
+            }
+        }
+    }
+}
